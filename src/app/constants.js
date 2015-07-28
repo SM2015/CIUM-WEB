4bf04c4de7db24af8e4cc6964955fb773a3811b9
Eliecer Ramírez <ramirez.esquinca@gmail.com>
2015-07-28 12:09:59 -0500

    configuracion rutas variables globales

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,11 +1,5 @@
 (function(){
-	'use strict';
-	angular.module('App').constant('URLS', {
-    	BASE: 'http://localhost/SSA_MATERIAL/APIRESTfull/public/',
-    	BASE_API: 'http://localhost/SSA_MATERIAL/APIRESTfull/public/api/v1/',
-		OAUTH_SERVER: 'http://localhost/SSA_MATERIAL/oauth2-server/public'
-   	});	
-	   
+	'use strict';	   
 	angular.module('App').constant('MENU',[
 { 
     grupo: false,
---
+++ src/app/urls.js
@@ -0,0 +1,8 @@
+(function(){
+	'use strict';
+	angular.module('App').constant('URLS', {
+    	BASE: 'http://localhost/SSA_MATERIAL/APIRESTfull/public/',
+    	BASE_API: 'http://localhost/SSA_MATERIAL/APIRESTfull/public/api/v1/',
+		OAUTH_SERVER: 'http://localhost/SSA_MATERIAL/oauth2-server/public'
+   	});	
+})();
